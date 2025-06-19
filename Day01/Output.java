e05841203df1f7c54e0c61189cadef90a5cffd1b
Alok <paseasy5@gmail.com>
2025-06-19 18:42:21 +0530

    Create Output.java

---
+++ Day01/Output.java
@@ -0,0 +1,7 @@
+public class Output {
+	public static void main(String[] args) {
+		System.out.print("hello , how are you ")
+    System.out.println("hey ...")
+    System.out.print("hey its breaking \n")
+	}
+}
