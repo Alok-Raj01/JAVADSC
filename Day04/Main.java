4d287d11e77a2fc63930b60e9e2843bf75b5c697
Alok <paseasy5@gmail.com>
2025-06-20 16:17:39 +0530

    Create Main.java

---
+++ Day04/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		// heres the if else statement 
+		if (5 >=5){
+			System.out.print("this is true");
+		}else {
+			System.out.print("this is false");
+		}
+		
+		// heres the switch statement 
+		switch(2){
+			case 1 : System.out.print("one");
+			break;
+			case 2 : System.out.print("two");
+			break;
+			default : System.out.print("its default");
+		}
+	}
+}
